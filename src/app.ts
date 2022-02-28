300e814246b2eea47d95f8b3e534273b72f523bb
sarmad <sarmadkamalkhan@gmail.com>
2022-02-28 16:12:17 +0500

    eslint

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
-import  stock from "../db/stock.json"
+import stock from "../db/stock.json"
 import transaction from "../db/transactions.json"
 
-function calculateData (sku: string)  {
-var totalStock:number, stockValue: { stock: number; sku: string; }, finalStock: number;
-stockValue = stock.find(function (element) { return element.sku == sku; });
-if(stockValue== undefined){return "No Records" }
-totalStock = stockValue.stock;
-for (var i = 0; i < transaction.length - 1; i++) {
-      transaction[i].sku == sku ? (transaction[i].type == "refund" ? (totalStock = totalStock + transaction[i].qty) : (totalStock = totalStock - transaction[i].qty)) : transaction[i].sku;
-      finalStock = totalStock;
-  }
-  stockValue.stock = finalStock;
-  stockValue.stock < 0 ? (stockValue.stock = 0) : stockValue.stock;
-  return stockValue
-  // console.log(stockValue);
+
+function calculateData(sku: string): object {
+    let totalStock: number, finalStock: number
+    const stockValue = stock.find(function(element): boolean {
+        return element.sku == sku;
+    },[])
+    if (stockValue == undefined) {
+        return null
+    }
+    totalStock = stockValue.stock
+    for (let i = 0; i < transaction.length - 1; i++) {
+        transaction[i].sku == sku ? (transaction[i].type == "refund" ? (totalStock = totalStock + transaction[i].qty) : (totalStock = totalStock - transaction[i].qty)) : transaction[i].sku
+        finalStock = totalStock
+    }
+    stockValue.stock = finalStock
+    stockValue.stock < 0 ? (stockValue.stock = 0) : stockValue.stock
+    return stockValue
+    // console.log(stockValue)
 }
 
 export default calculateData
--- test/src/App.test.ts
+++ test/src/App.test.ts
@@ -4,25 +4,25 @@ import calculateData from "../../src/App"
 //DTW874360/97/81
 //LTV719449/39/39
 
-describe('Stock-level working',()=>{
-    it('If given value not in records',()=>{
+describe('Stocklevel working',(): void=>{
+   
+    it('If given value not in records',(): void=>{
         expect(calculateData("LTV719449/39/")
-        ).toBe("No Records");
+        ).toBe(null);
     })
-    it('If stocks Empty after transactions',()=>{
+    it('If stocks Empty after transactions',(): void=>{
         expect(calculateData("DTW874360/97/81")
         ).toEqual({"sku": "DTW874360/97/81", "stock": 0});
     })
-    it('If give value it should calculate',()=>{
+    it('If give value it should calculate',(): void=>{
         expect(calculateData("LTV719449/39/39")
         ).toEqual({"sku": "LTV719449/39/39", "stock": 8510});
     })
-    it('If give value it should give object',()=>{
+    it('If give value it should give object',(): void=>{
         expect(typeof {calculateData: 'LTV719449/39/39'}).toBe('object');
     })
-    it('following signature: (sku: string) => Promise<{ sku: string, qty: number }',()=>{
+    it('following signature: (sku: string) => Promise<{ sku: string, qty: number }',(): void=>{
         expect(calculateData("LTV719449/39/39")
         ).toEqual({"sku":expect.any(String), "stock":expect.any(Number)});
     })
-   
 })
