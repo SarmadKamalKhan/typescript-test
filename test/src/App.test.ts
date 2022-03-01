2bfddbc7729d55b7885201499a9158efec0c2b5b
sarmad <sarmadkamalkhan@gmail.com>
2022-03-01 16:24:29 +0500

    poc-promis-return-2

--- test/src/App.test.ts
+++ test/src/App.test.ts
@@ -8,8 +8,7 @@ describe('Stocklevel working',(): void=>{
    
     it('If given value not in records', async(): Promise<void> =>{
         const x= await calculateData("LTV719449/39/");
-        expect(x).toBe(null);
-        
+        expect(x).toBe(null);   
     }) 
     it('If stocks Empty after transactions',(): void=>{
         expect(calculateData("DTW874360/97/81")
@@ -22,8 +21,9 @@ describe('Stocklevel working',(): void=>{
     it('If give value it should give object',(): void=>{
         expect(typeof {calculateData: 'LTV719449/39/39'}).toBe('object');
     })
-    it('following signature: (sku: string) => Promise<{ sku: string, qty: number }',(): void=>{
-        expect(calculateData("LTV719449/39/39")
-        ).toEqual({"sku":expect.any(String), "stock":expect.any(Number)});
-    })
+    it('following signature: (sku: string) => Promise<{ sku: string, qty: number }', async(): Promise<void> =>{
+        const x= await calculateData("LTV719449/39/39");
+        expect(x).toEqual({"sku":expect.any(String), "stock":expect.any(Number)});
+    }) 
+   
 })
